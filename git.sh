4bc202905e92de3a62ea750baa454d3a766f58dc
alex <louyuankang@osase.net>
2024-07-31 16:15:53 +0800

    1

--- git.sh
+++ git.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -u  # 开启未设置变量检测
+set -e  # 开启错误检测
 
 echo "提交代码中请稍等..."
 pink_bg='\033[44;37m'
 err_color='\033[38;5;206m'
-git pull
+git pull || { echo -e "$err_color Git pull 失败 \033[0m"; exit 1; }
 
 git add .
 
@@ -15,12 +16,12 @@ if [ -z "$commit_context" ]; then
     exit 1
 fi
 
-git commit -m "$commit_context"
+git commit -m "$commit_context" || { echo -e "$err_color Git commit 失败 \033[0m"; exit 1; }
 
-git push
+git push || { echo -e "$err_color Git push 失败 \033[0m"; exit 1; }
 
 echo "代码提交完成。"
-echo -e "\033{ \033[5m"
-echo -e "$pink_bg ********************************************************* \033[5m"
-echo -e "$pink_bg 🚀********************代码提交完成。********************🚀 \033[5m"
-echo -e "$pink_bg ********************************************************* \033[5m"
+echo -e "\033[5m"
+echo -e "$pink_bg ********************************************************* \033[0m"
+echo -e "$pink_bg 🚀*******************代码提交完成。********************🚀 \033[0m"
+echo -e "$pink_bg ********************************************************* \033[0m"
