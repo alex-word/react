9a67020252bb941904f7d49d7b1230cd673b97e2
admin <l13333717069@163.com>
2024-09-26 17:59:03 +0800

    修改shell

--- git.sh
+++ git.sh
@@ -7,20 +7,19 @@ attempt=0  # 初始化尝试次数为0
 
 while [ $attempt -lt $max_attempts ]; do
     echo "提交代码中请稍等..."
-    pink_bg='\033[44;37m'
+    text_bg='\033[44;37m'
     err_color='\033[38;5;206m'
     git pull
 
     git add .
-
     # 内层循环用于获取有效的提交内容
     while true; do
         read -p "请输入提交内容: " commit_context
 
         if [ -z "$commit_context" ]; then
-            echo -e "$err_color 请输入commit内容 \033[0m"
             attempt=$((attempt + 1))
             if [ $attempt -lt $max_attempts ]; then
+                echo -e "$err_color 请输入commit内容 \033[0m"
                 remaining_attempts=$((max_attempts - attempt))
                 echo "你还有 $remaining_attempts 次尝试机会。"
                 # 继续内层循环，让用户重新输入
@@ -36,9 +35,9 @@ while [ $attempt -lt $max_attempts ]; do
         if git push; then
             echo "代码提交完成。"
             echo -e "\033{ \033[5m"
-            echo -e "$pink_bg ********************************************************* \033[0m"
-            echo -e "$pink_bg 🚀*******************代码提交完成。********************🚀 \033[0m"
-            echo -e "$pink_bg ********************************************************* \033[0m"
+            echo -e "$text_bg ********************************************************* \033[0m"
+            echo -e "$text_bg 🚀*******************代码提交完成。********************🚀 \033[0m"
+            echo -e "$text_bg ********************************************************* \033[0m"
             exit 0
         else
             echo -e "$err_color 提交失败，请检查问题并重试。 \033[0m"
